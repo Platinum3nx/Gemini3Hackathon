4e9d7eae737c1b0e5210d532477d3f8e52bdd7d9
Platinum3nx <amalghan70@gmail.com>
2026-01-21 00:42:32 -0500

    Add support for GEMINI_API_KEY in entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# Map the GitHub Action input to the variable Argus expects
+if [ -n "$INPUT_GEMINI_API_KEY" ]; then
+    export GEMINI_API_KEY="$INPUT_GEMINI_API_KEY"
+fi
+
 if [ "$#" -gt 0 ]; then
     exec "$@"
 fi
